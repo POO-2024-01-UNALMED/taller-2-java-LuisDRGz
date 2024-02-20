abe4b03c312549ddb22721ec91651e731aa09001
LuisDRGz <luiramirezgo@unal.edu.co>
2024-02-19 19:46:14 -0500

    Modificación-05

--- src/main/java/Motor.java
+++ src/main/java/Motor.java
@@ -6,7 +6,7 @@ public class Motor {
 	public static int registro;
 
 	public void cambiarRegistro(int registro) {
-		this.registro = registro;
+		Motor.registro = registro;
 	}
 
 	public void asignarTipo(String tipo) {
